c726c54279bb3dd24cf68df6903b879e006803b3
WandaSbl <wanda.sebille@lilo.org>
2024-12-29 18:30:20 +0100

    Connexion à la base de données

---
+++ supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://jxamvxyxyrncpqbslgfa.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4YW12eHl4eXJuY3BxYnNsZ2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0ODE0MTYsImV4cCI6MjA1MTA1NzQxNn0.x0AUDQaO_yqNdO2pnhbVUAq3f_aIN8CSpgOl9A9n8Jo';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+//database password : gonebrassed69
